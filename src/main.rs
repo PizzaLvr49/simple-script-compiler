e6bb89394ec6376a4ddbe991ab43e21e8b41e07f
ExoticSliceOfPizza <136937744+PizzaLvr49@users.noreply.github.com>
2025-09-19 23:38:32 -0400

    e

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ pub mod lexer;
 pub mod parser;
 
 fn main() {
-    let source = "var myStr = \"Hello, Rust!\"; println(myStr); var myNum = 4.28; println(myNum); println(\"Type of myNum:\", typeof(myNum));";
+    let source =
+        "var myStr = \"Hello, Rust!\"; println(myStr); var myNum = 4.28; println(myNum); println(\"Type of myNum:\", typeof(myNum));";
 
-    // Parse the source code
     let lexer = lexer::Lexer::new(source);
     let mut parser = parser::Parser::new(lexer);
 
