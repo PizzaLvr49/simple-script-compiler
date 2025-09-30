486a9451ba700cbacf4587fe0a8836997f3de446
ExoticSliceOfPizza <136937744+PizzaLvr49@users.noreply.github.com>
2025-09-29 21:39:54 -0400

    Add CI workflow for building and publishing documentation

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-pub mod interpreter;
-pub mod lexer;
-pub mod parser;
+use simple_script_compiler::{Interpreter, Lexer, Parser};
 
 fn main() {
     let source = r#"
-    println(2 * 4 + (3 + 6 + -7 * 2) / 2);
+    var x = 32424 * 312;
+    println(2 * 4 + (3 + 6 + -7 * 2) / 2 + x - -3 * (x * x * x * x * x));
     "#;
 
-    let lexer = lexer::Lexer::new(source);
-    let mut parser = parser::Parser::new(lexer);
+    let lexer = Lexer::new(source);
+    let mut parser = Parser::new(lexer);
 
     match parser.parse() {
         Ok(program) => {
-            let mut interpreter = interpreter::Interpreter::new();
+            let mut interpreter = Interpreter::new();
 
             if let Err(runtime_error) = interpreter.interpret(program) {
                 println!("Runtime error: {runtime_error}");
